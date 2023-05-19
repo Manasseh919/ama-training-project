b77db3edae6bfea76efe8d2747463451b4b83b53
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-05-19 11:36:02 +0000

    completed most of the ui with feedbad details done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-
-import Roadmap from "./pages/Roadmap";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import RoadMap from './pages/roadmap';
+import AddFeedback from './pages/feedback/add-feedback';
+import NotFound from './pages/notFound';
+import SingleFeedbackDetail from './pages/feedback-detail/singleFeedbackDetail';
 
 function App() {
-  return (
-  <div>
-    <main>
-      <Roadmap/>
-    </main>
-    </div>
-  );
+   return (
+      <>
+         <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/add-feedback' element={<AddFeedback />} />
+            <Route exact path='/roadmap' element={<RoadMap />} />
+            <Route
+               exact
+               path='/feedback-detail/:id'
+               element={<SingleFeedbackDetail />}
+            />
+            <Route path='*' element={<NotFound />} />
+         </Routes>
+      </>
+   );
 }
 
 export default App;
