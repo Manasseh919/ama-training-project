6603887ba9f88983f6575d94d96c2bb0a4b8d7f2
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-05-22 10:20:16 +0000

    i added the form slices

--- src/data/formSelect.js
+++ src/data/formSelect.js
@@ -1,15 +1,22 @@
 export const sortBtns = [
-    { id: 1, type: 'Most Upvotes' },
-    { id: 2, type: 'Least Upvotes' },
-    { id: 3, type: 'Most Comments' },
-    { id: 4, type: 'Least Comments' },
- ];
- 
- export const categories = [
-    { id: 1, type: 'All' },
-    { id: 2, type: 'UI' },
-    { id: 3, type: 'UX' },
-    { id: 4, type: 'Enhancement' },
-    { id: 5, type: 'Feature' },
-    { id: 6, type: 'Bug' },
- ];
+   { id: 1, type: 'Most Upvotes' },
+   { id: 2, type: 'Least Upvotes' },
+   { id: 3, type: 'Most Comments' },
+   { id: 4, type: 'Least Comments' },
+];
+
+export const categories = [
+   { id: 1, type: 'all' },
+   { id: 2, type: 'ui' },
+   { id: 3, type: 'ux' },
+   { id: 4, type: 'enhancement' },
+   { id: 5, type: 'feature' },
+   { id: 6, type: 'bug' },
+];
+
+export const statuses = [
+   { id: 1, type: 'suggestion' },
+   { id: 2, type: 'planned' },
+   { id: 3, type: 'in-progress' },
+   { id: 4, type: 'live' },
+];
